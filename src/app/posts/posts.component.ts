3b150d19e29890df26b35328a3fd78141764a717
Levani Birchadze <levanbirchadze@gmail.com>
2023-08-12 15:47:17 +0400

    fix route

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-posts',
@@ -9,8 +10,13 @@ import { Router } from '@angular/router';
 })
 export class PostsComponent {
   posts: any[] = [];
+  userId: number = 1;
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
     this.http
@@ -21,7 +27,7 @@ export class PostsComponent {
   }
 
   onGoBackClick(): void {
-    this.router.navigate(['/']);
+    this.location.back();
   }
   onMainPageClick(): void {
     this.router.navigate(['/']);
