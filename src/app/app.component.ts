29d2de0de87688e515a3bbb6d9c2ad9193fb9198
Levani Birchadze <levanbirchadze@gmail.com>
2023-08-12 11:10:43 +0400

    get data from api and display on table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'crocobet-task';
